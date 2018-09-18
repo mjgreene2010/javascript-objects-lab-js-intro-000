c7c2c58b04b077881bf67ccc84335e75de71d56b
mjgreene2010 <mjgreene2010@gmail.com>
2018-09-18 20:17:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  return delete object[key]
-  
+  //return delete object[key]
+  object[key] = undefined
   
 }
